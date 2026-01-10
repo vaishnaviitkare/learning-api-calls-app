9013287aabf60959df9386ed2f8db277a643fa2b
vaishnaviitkare <vaishuitkare@gmail.com>
2026-01-10 14:41:31 +0530

    public api is used and fetch the movies through api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import MoviesList from './Components/MoviesList';
 function App() {
+  const[movies,setMovies]=useState([]);
+   const fetchMoviesHandler=()=>{
+    //here i use ghibli api
+    fetch("https://ghibliapi.vercel.app/films")
+         .then((response)=>{
+           return response.json();//converted json text to js
+         })
+         .then((data)=>{
+          //here the data we get has different key name so we store this as per our name//
+          const transformedmovies=data.map((movieData)=>{
+            return{
+              id:movieData.id,
+              title:movieData.title,
+              openingText:movieData.description,
+              releaseDate:movieData.release_date
+            
+            }
+            
+          })
+           setMovies(transformedmovies);
+         })
+        
+        }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <React.Fragment>
+  <div className={"movies-body"}>
+  <section className={"fetchmovies"}>
+    <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+  </section>
+  <section className={"display-movies"}>
+    <MoviesList movies={movies}/>
+  </section>
+  </div>
+ </React.Fragment>
   );
 }
 
