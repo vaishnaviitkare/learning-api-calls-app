354bc9de834a4a461cbd015e47eb17d2e0b394e3
vaishnaviitkare <vaishuitkare@gmail.com>
2026-01-12 13:30:15 +0530

    whenever the api fails, retry calling the api again and again after intervals of 5 seconds , uptill the time it passes and cancel button added to cancel retry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import MoviesList from './Components/MoviesList';
 function App() {
   const[movies,setMovies]=useState([]);
   const[isLoading,setIsLoading]=useState(false);
+  const[error,setError]=useState(null);
+ const retryRef=useRef(true);
   //use of async await because of then blocks
-   const fetchMoviesHandler=async()=>{
+   //helper function for delay
+  const delay=(ms)=>new Promise((resolve)=>setTimeout(resolve,ms));
+  const fetchMoviesHandler=async()=>{
     setIsLoading(true);
+    //here we set error null because when we click on button if any error left it become null
+    setError(null);
+    let success=false;
+    retryRef.current=true
+    while(!success && retryRef){
+    try{
     //here i use ghibli api
     const response=await fetch("https://ghibliapi.vercel.app/films")
+    if(!response.ok){
+      throw new Error('Something went wrong ....Retrying');
+    }
     const data=await response.json();//converted json text to js
           //here the data we get has different key name so we store this as per our name//
           const transformedmovies=data.map((movieData)=>{
@@ -22,8 +35,21 @@ function App() {
             
           })
            setMovies(transformedmovies);
-            setIsLoading(false);
+           success=true;
         }
+        catch(error){
+         setError(error.message);
+         if(retryRef){
+         await delay(5000);
+         }
+      }
+    }
+       setIsLoading(false);
+    }
+    const cancelRetryHandler=()=>{
+       retryRef.current=false;
+       setIsLoading(false);
+    }
   return (
  <React.Fragment>
   <div className={"movies-body"}>
@@ -32,9 +58,15 @@ function App() {
   </section>
   <section className={"display-movies"}>
    {!isLoading && movies.length>0 && <MoviesList movies={movies}/>}
-   {!isLoading && movies.length==0 && <p>Found no movies</p>}
-   {isLoading && <p>Loading...</p>}
-  </section>
+   {!isLoading && movies.length==0 && !error && <p className={"nofound"}>Found no movies</p>}
+   {!isLoading && error && <p className={'error'}>{error}</p>}
+   {isLoading && 
+   (<div className={"loading-text"}>
+    <p>Loading...</p>
+    <button onClick={cancelRetryHandler}>Cancel retrying</button>
+    </div>)}
+  
+    </section>
   </div>
  </React.Fragment>
   );
